e6e192a090ccb29fde08c1a3f0974089e180524d
wglockner <walter.w.glockner@gmail.com>
2025-08-22 14:22:36 -0500

    Added testing files

--- khi2cpp_hw/bringup/launch/cx165l_controller.launch.py
+++ khi2cpp_hw/bringup/launch/cx165l_controller.launch.py
@@ -39,9 +39,8 @@ def generate_launch_description():
             " ",
             PathJoinSubstitution(
                 [
-                    FindPackageShare("khi2cpp_hw_description"),
-                    "cx165l/urdf",
-                    "cx165l.urdf.xacro",
+                    FindPackageShare("snp_isu_support"),
+                    "urdf/workcell.xacro",
                 ]
             ),
         ]
